0f9236f6d3f9848e0e91e84aa046f4559ca478e9
Lucas Sousa <40129564+LucasSousaO@users.noreply.github.com>
2020-08-04 13:12:48 -0300

    Update logo.js

--- frontend/src/pages/login/logo/logo.js
+++ frontend/src/pages/login/logo/logo.js
@@ -2,6 +2,6 @@ import React from "react";
 
 export const Logo = () => (
   <div className="row justify-content-center align-items-center">
-    <img src="/images/logo2.svg" />
+    <img src="/images/logo.svg" />
   </div>
 );
