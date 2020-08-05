26a6c541fb7d584599624d334c6de24adc322867
Lucas Sousa <40129564+LucasSousaO@users.noreply.github.com>
2020-08-05 08:46:35 -0300

    Update logo.js

--- frontend/src/pages/login/logo/logo.js
+++ frontend/src/pages/login/logo/logo.js
@@ -3,5 +3,6 @@ import React from "react";
 export const Logo = () => (
   <div className="row justify-content-center align-items-center">
     <img src="/images/logo.svg" />
+    <img src={'https://ssvpbrasil.org.br/wp-content/uploads/2018/09/logo.jpg'} alt="ssvp" />
   </div>
 );
